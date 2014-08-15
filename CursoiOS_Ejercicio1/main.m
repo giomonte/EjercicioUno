ce3c3d71f2078c5e3dad211be991645d983f35b7
CCDM20 <LABORATORIOMAC@iMac-de-CCDM20.local>
2014-08-15 10:08:48 -0600

    Initial Commit

---
+++ CursoiOS_Ejercicio1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CursoiOS_Ejercicio1
+//
+//  Created by CCDM20 on 15/08/14.
+//  Copyright (c) 2014 CCDM20. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
